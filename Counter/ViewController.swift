1a9c2b833c6a88b24339d47b6ccc84fd60d47c9b
IrinaV <vasilyeva.irina18@gmail.com>
2024-07-17 15:08:33 +0300

    Счетчик готов

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelMain: UILabel!
+    
+    @IBOutlet weak var buttonMain: UIButton!
+    
+    private var buttonTap: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        buttonTap += 1
+        labelMain.text = "Значение счетчика: \(buttonTap)"
+    }
+    
 }
 
